9d754044c2c927e4ce7ab20f0852dbfba0750836
sanda03 <hei.sanda.2@gmail.com>
2024-06-24 13:31:52 +0300

    feat: get pointages du mois

--- src/main/java/com/sanda/pointage/Employee.java
+++ src/main/java/com/sanda/pointage/Employee.java
@@ -4,17 +4,49 @@ import lombok.AllArgsConstructor;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-@AllArgsConstructor
 public class Employee{
     private String nom;
-    private String prenoms,;
+    private String prenoms;
     private String cin;
     private LocalDate dateDeNaissance;
     private LocalDate dateEmbauche;
     private LocalDate finContrat;
     private Categorie categorie;
-    public BigDecimal getSalaire(){
-        return BigDecimal.TEN;
+    private Map<Month, List<Pointage>> pointages = new HashMap<>();
+
+    public Employee(LocalDate finContrat, String nom, String prenoms, String cin, LocalDate dateDeNaissance, LocalDate dateEmbauche, Categorie categorie) {
+        this.finContrat = finContrat;
+        this.nom = nom;
+        this.prenoms = prenoms;
+        this.cin = cin;
+        this.dateDeNaissance = dateDeNaissance;
+        this.dateEmbauche = dateEmbauche;
+        this.categorie = categorie;
+    }
+
+    public BigDecimal getSalaire(Month mois){
+        var pointageDuMois = pointages
+            .entrySet()
+            .stream()
+            .filter(pointage -> pointage.getKey().equals(mois))
+            .map(pointage -> pointage.getValue())
+            .reduce(new ArrayList<>(),  (acc, pointage) -> {
+                acc.addAll(pointage);
+                return acc;
+            });
+
+
+    }
+
+    void addPointage(Month mois, Pointage pointage){
+        var pointagesAvant = pointages.getOrDefault(mois, new ArrayList<>());
+        pointagesAvant.add(pointage);
+        pointages.put(mois, pointagesAvant);
     }
 }
--- src/main/java/com/sanda/pointage/Pointage.java
+++ src/main/java/com/sanda/pointage/Pointage.java
@@ -1,5 +1,20 @@
 package com.sanda.pointage;
 
-import java.time.LocalTime;
+import lombok.Data;
 
-public record Pointage(Employee person, LocalTime debut, LocalTime fin) {}
+import java.time.LocalTime;
+import java.time.Month;
+
+@Data
+public class Pointage{
+    private Employee person;
+    private LocalTime debut;
+    private LocalTime fin;
+
+    public Pointage(Month mois, Employee person, LocalTime debut, LocalTime fin) {
+        this.person = person;
+        this.debut = debut;
+        this.fin = fin;
+        this.person.addPointage(mois, this);
+    }
+}
